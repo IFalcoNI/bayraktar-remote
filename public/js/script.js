fc48ba0c0bbcc1fda9ae01a4069d9971363f083f
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2022-10-19 05:13:42 +0300

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,31 @@
-const makeTurn = (direction) => {
-    let dir = document.getElementById('direction')
-    dir.innerHTML = direction
-}
+
 $('form').on('submit', function (e) {
     e.preventDefault();
 });
 $(document).ready(() => {
-    $("#forward").click(() => {
-        $.post("/forward", {
-            cmd: "forward"
+    $('#forward').click(() => {
+        $.post('/forward', {
+            cmd: 'forward'
         })
     });
-    $("#back").click(() => {
-        $.post("/back", {
-            cmd: "back"
+    $('#back').click(() => {
+        $.post('/back', {
+            cmd: 'back'
         })
     });
-    $("#left").click(() => {
-        $.post("/left", {
-            cmd: "left"
+    $('#left').click(() => {
+        $.post('/left', {
+            cmd: 'left'
         })
     });
-    $("#right").click(() => {
-        $.post("/right", {
-            cmd: "right"
+    $('#right').click(() => {
+        $.post('/right', {
+            cmd: 'right'
         })
     });
-    $("#fire").click(() => {
-        $.post("/fire", {
-            cmd: "fire"
+    $('#fire').click(() => {
+        $.post('/fire', {
+            cmd: 'fire'
         })
     });
 
