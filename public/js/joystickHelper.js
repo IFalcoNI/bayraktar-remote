72ed5afcdd645fb3baabdbb0885df3b47190f8b2
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2022-10-20 13:39:08 +0300

    Added Joystick

--- public/js/joystickHelper.js
+++ public/js/joystickHelper.js
@@ -1,5 +1,7 @@
 let joy3Param = { "title": "joystick3" };
-let Joy3 = new JoyStick('joy3Div', joy3Param);
-// setInterval(function () { sendPost(`${Joy3.GetPosX()}-${Joy3.GetPosY()}`) }, 200);
-// setInterval(function () { sendPost(`${Joy3.GetPosX()}-${Joy3.GetPosY()}`) }, 200);
-setInterval(function () { sendPost(Joy3.getDir()) }, 200);
+let Joy3 = new JoyStick('joy3Div', joy3Param); let intervalId;
+const handleJoystick = () => {
+    intervalId = setInterval(function () { sendPost(`${Joy3.GetX()}|${Joy3.GetY()}`) }, 200);
+}
+handleJoystick()
+      // setInterval(function(){sendPost(Joy3.GetDir())}, 300);
